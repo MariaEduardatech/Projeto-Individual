f8eba5381c99fa3b1867bf99f8dfe835db244dff
MariaEduardatech <maria.paula@sptech.school>
2025-06-01 14:54:55 -0300

    documentação

--- site/src/models/quizModel.js
+++ site/src/models/quizModel.js
@@ -8,12 +8,10 @@ console.log("executando a intrução SQL: \n" + instrucaoSql);
 return database.executar(instrucaoSql);
 }
 
-// Coloque os mesmos parâmetros aqui. Vá para a var instrucaoSql
+
 function guardar(dataResposta, resultado) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", dataResposta, resultado);
-    
-    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
-    //  e na ordem de inserção dos dados.
+ 
     var instrucaoSql = `
         INSERT INTO quiz (dataResposta, resultado) VALUES ('${dataResposta}', '${resultado}');
     `;
