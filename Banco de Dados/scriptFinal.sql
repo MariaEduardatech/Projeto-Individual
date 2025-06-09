1765f29ad9444c3b4fa98328756daaae9a1470f2
MariaEduardatech <maria.paula@sptech.school>
2025-06-09 19:36:20 -0300

    script e modelagem finais

---
+++ Banco de Dados/scriptFinal.sql
@@ -0,0 +1,31 @@
+Create database Projeto_individual;
+
+USE Projeto_individual;
+
+CREATE TABLE usuario (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(45),
+    email VARCHAR(45),
+    senha VARCHAR(45)
+);
+
+CREATE TABLE JogoMemoria (
+    idJogo INT PRIMARY KEY AUTO_INCREMENT,
+    dataPartida DATETIME DEFAULT CURRENT_TIMESTAMP, 
+    acertos INT,
+    erros INT,
+    fkusuario INT,
+    FOREIGN KEY (fkusuario) REFERENCES usuario(id)
+);
+
+CREATE TABLE Quiz (
+    idQuiz INT PRIMARY KEY AUTO_INCREMENT,
+    Dataresposta DATETIME DEFAULT CURRENT_TIMESTAMP,
+    resultado VARCHAR(150),
+    fkusuario INT,
+    FOREIGN KEY (fkusuario) REFERENCES usuario(id)
+);
+
+select * from quiz;
+select * from usuario;
+select * from jogomemoria;
